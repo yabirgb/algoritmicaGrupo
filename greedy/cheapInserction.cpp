8a9650f30ee85ea0d2adf751c5a3d6c9b53128ba
Yábir <yabirgb@gmail.com>
2018-04-25 18:55:06 +0200

    Initial commit

---
+++ greedy/cheapInserction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int N; //Total number of cities
+
+  int **cities = new *int[N];
+
+  for(int i = 0; i < N; i++){
+    cities[i] = new int[N];
+  }
+  
+}
