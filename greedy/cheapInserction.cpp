a7679f323aa0ff03115de04923f2046aceb022ea
Yabir <yabirgb@gmail.com>
2018-04-28 14:30:42 +0200

    Detalles de la solucion

--- greedy/cheapInserction.cpp
+++ greedy/cheapInserction.cpp
@@ -9,10 +9,12 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
+#define prod
+
 using namespace std;
 
 
-void printMatrix(vector<vector<double>> matrix){
+void printMatrix(vector<vector<double> > matrix){
   for(int i=0; i < matrix.size(); i++){
     for(int j=0; j < matrix[i].size(); j++){
       printf("%.4f ", matrix[i][j]);
@@ -24,14 +26,14 @@ void printMatrix(vector<vector<double>> matrix){
 template <typename T>
 void printVector(vector<T> v){
 
-  cout << "vector = [";
+  cout << "[";
   for(int i =0; i < v.size()-1; i++){
     cout << v[i] << ",";
   }
 
   cout << v[v.size()-1];
 
-  cout << "]" << endl;
+  cout << "]";
 }
 
 
@@ -39,7 +41,7 @@ double distance(pair<double, double> c1, pair<double, double> c2){
   return hypot(c2.first - c1.first, c2.second - c1.second);
 }
 
-int closest_element(int pos, vector<double> v){
+int closest_element(int pos, vector<double> &v){
   int shortest = 0;
   int value = v[0];
 
@@ -47,7 +49,7 @@ int closest_element(int pos, vector<double> v){
     shortest = 1;
     value = v[1];
   }
-  
+
   for(int i=0; i < v.size(); i++){
     if (v[i] < value && i != pos){
       value = v[i];
@@ -58,34 +60,45 @@ int closest_element(int pos, vector<double> v){
   return shortest;
 }
 
-double compute_addition(int p1, int p2, int p3, vector<vector<double>>&matrix){
+double compute_addition(int p1, int p2, int p3, vector<vector<double> >&matrix){
   return matrix[p3][p1] + matrix[p2][p3] - matrix[p1][p2];
 }
 
-vector<int> add_closet_to_path(vector<int> path, int N, vector<vector<double>>& matrix){
+void add_closet_to_path(vector<int> &path, vector<int> &candidatos, int N, vector<vector<double> >& matrix){
   double best_dist = numeric_limits<double>::max();
+  double best_dist_global = numeric_limits<double>::max();
+
   double dist;
-  int best_pos=0;
-  vector<int> new_path(path);
-  
+  int best_city_global;
+
+  int best_pos_global;
+  int best_pos;
+  int city;
   //For every city
-  for(int city=0; city < N; city++){
-    //If not in the path for every city
-    for(int j=0; j < new_path.size()-1 && path[0] != city && path[1] != city; ++j){
-      dist = compute_addition(new_path[j], new_path[j+1], city, matrix);
-      //Test if is the best match
+  for(int i=0; i < candidatos.size(); i++){
+    city = candidatos[i];
+
+    //If not in the path compute the best position for the city
+    for(int j=0; j < path.size()-1; ++j){
+      dist = compute_addition(path[j], path[j+1], city, matrix);
+
       if (dist < best_dist){
-	best_dist = dist;
-	best_pos = j;
+        best_dist = dist;
+        best_pos = j;
       }
     }
-    new_path.insert(new_path.begin()+best_pos, city);
-  }
 
-  return new_path;
+    if (best_dist < best_dist_global){
+      best_pos_global = best_pos;
+      best_city_global = i;
+      best_dist_global = best_dist;
+    }
+  }
+  path.insert(path.begin()+best_pos_global+1,candidatos[best_city_global]);
+  candidatos.erase(candidatos.begin() + best_city_global);
 }
 
-double compute_length(vector<int> path, vector<vector<double>> &cities){
+double compute_length(vector<int> path, vector<vector<double> > &cities){
   double result = 0;
   for(int i = 0; i < path.size()-1; i++){
     result += cities[path[i]][path[i+1]];
@@ -105,27 +118,25 @@ int main(int argc, char **argv){
   string trash;
   vector<int> result; //Vector of integers representing the order of cities
   int initial;
+  double dist;
 
   if(argc != 3){
     cerr << "Error in the number of arguments" << endl;
     return 1;
   }
-  
+
   //Open the file
   filename = argv[1];
   ifstream data(filename);
   data >> trash;
   data >> N; //First line is always the cardinal of cities
 
-  int mini, minj;
-  double dist, best=numeric_limits<int>::max();
-
   //Create a vector of 3-uplas
 
-  vector<pair<double, double>> coordinates;
-  
+  vector<pair<double, double> > coordinates;
+
   //Create an square matrix
-  vector<vector<double>> cities(N, vector<double>(N,0));
+  vector<vector<double> > cities(N, vector<double>(N,0));
 
   //Store all the input data
   for(int i=0; i < N; i++){
@@ -138,31 +149,40 @@ int main(int argc, char **argv){
       dist = distance(coordinates[i], coordinates[j]);
       cities[i][j] = dist;
       cities[j][i] = cities[i][j];
-
-      if(dist < best){
-	mini = i;
-	minj = j;
-	best = dist;
-      }
     }
-
-    best=numeric_limits<int>::max();
   }
-  cout << N << endl;
   //printMatrix(cities);
 
   //Start with a random node
-  //srand (time(NULL));
-  //initial = rand()%(N);
-  result.push_back(mini);
+  srand (time(NULL));
+  initial = rand()%(N);
+  initial = 0;
+  result.push_back(initial);
+  result.push_back(closest_element(initial, cities[initial]));
+  std::vector<int> candidatos;
 
-  //Search for the nearest node and add it to the circuit
-  //int latest = result[result.size()-1];
-  //next_node = closest_element(latest, cities[latest]);
-  result.push_back(minj);
-  result = add_closet_to_path(result,N, cities);
-  //printVector(result);
+  for(int i = 0; i < N; i++){
+    if(i!=initial && i!=result[1])
+      candidatos.push_back(i);
+  }
+
+  clock_t tStart = clock();
+  while(!candidatos.empty()){
+    add_closet_to_path(result,candidatos, N, cities);
+  }
+  clock_t finish = clock();
+
+  #ifdef dev
+  cout << "Ciudades: ";
+  printVector(result);
+  cout << " Total: " << result.size() << endl;
   cout << "Total distance: " << compute_length(result, cities) << endl;
+  #endif
+
+  #ifdef prod
+  cout << compute_length(result, cities) << "\t" << ((double)finish - tStart)/CLOCKS_PER_SEC << endl;
+  #endif
+
   ofstream out(output);
   int city;
   for(int i =0; i < result.size(); i++){
@@ -189,7 +209,12 @@ int main(int argc, char **argv){
     salida << city << " " << coordinates[city].first << " " << coordinates[city].second << endl;
   }
 
+  #ifdef dev
   cout << "La longitud de la solución ofrecida es: " << compute_length(ciudades, cities) << endl;
+  cout << "Solución ofrecida: ";
+  printVector(ciudades);
+  cout << endl;
+  #endif
 
   return 0;
 }
